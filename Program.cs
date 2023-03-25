941491b9ad910f39587f04c641676cb8b3ac3748
First <ddamage11@gmail.com>
2023-03-25 12:34:19 +0600

    cikl

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] array1 = new string[5] {"10" , "no" , "100" , "hello" , "world"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIf(string[] array1,string[] array2)
+{
+    int count = 0;
+    for( int i = 0; 1 < array.Length; i++)
+    {
+        if(array[i].Length <=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
