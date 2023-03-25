5b0e84fd01bd2b1d033439a763e8cfa1133c1b35
First <ddamage11@gmail.com>
2023-03-25 12:45:30 +0600

    final

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@ string[] array2 = new string[array1.Length];
 void SecondArrayWithIf(string[] array1,string[] array2)
 {
     int count = 0;
-    for( int i = 0; 1 < array.Length; i++)
+    for( int i = 0; 1 < array1.Length; i++)
     {
-        if(array[i].Length <=3)
+        if(array1[i].Length <=3)
         {
             array2[count] = array1[i];
             count++;
         }
     }
 }
+void PrintArray(string[]array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIf(array1, array2);
+PrintArray(array2);
